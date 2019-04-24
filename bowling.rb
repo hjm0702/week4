68f3a417de2e50f7fe7c0b8b9b16c209caafa9b5
Jeff Cohen <cohen.jeff@gmail.com>
2019-04-23 19:26:44 -0500

    At the end of bowling game kata

--- bowling.rb
+++ bowling.rb
@@ -1,3 +1,40 @@
 class Game
 
+  attr_accessor :total
+  attr_accessor :rolls
+
+  def initialize
+    self.total = 0
+    self.rolls = []
+  end
+
+  def roll(pins_knocked_down)
+    self.rolls.push(pins_knocked_down)
+  end
+
+  def strike?(rollIndex)
+    return self.rolls[rollIndex] == 10
+  end
+
+  def spare?(rollIndex)
+    return self.rolls[rollIndex] + self.rolls[rollIndex + 1] == 10
+  end
+
+  def score
+    rollIndex = 0
+    self.total = 0
+    10.times do
+      if strike?(rollIndex)
+        self.total += 10 + self.rolls[rollIndex + 1] + self.rolls[rollIndex + 2]
+        rollIndex += 1
+      elsif spare?(rollIndex)
+        self.total += 10 + self.rolls[rollIndex + 2]
+        rollIndex += 2
+      else
+        self.total += self.rolls[rollIndex] + self.rolls[rollIndex + 1]
+        rollIndex += 2
+      end
+    end
+    return self.total
+  end
 end
--- bowling_test.rb
+++ bowling_test.rb
@@ -4,18 +4,51 @@ require_relative 'bowling'
 class GameTest < Minitest::Test
 
   def test_gutter_game
+    game = Game.new
+    20.times do
+      game.roll 0
+    end
+
+    assert_equal 0, game.score
   end
 
   def test_all_ones
+    game = Game.new
+    20.times do
+      game.roll 1
+    end
+
+    assert_equal 20, game.score
   end
 
   def test_one_spare
+    game = Game.new
+    game.roll 6
+    game.roll 4
+    game.roll 3
+    17.times do
+      game.roll 0
+    end
+    assert_equal 16, game.score
   end
 
   def test_one_strike
+    game = Game.new
+    game.roll 10
+    game.roll 4
+    game.roll 3
+    16.times do
+      game.roll 0
+    end
+    assert_equal 24, game.score
   end
 
   def test_perfect_game
+    game = Game.new
+    12.times do
+      game.roll 10
+    end
+    assert_equal 300, game.score
   end
 
 end
